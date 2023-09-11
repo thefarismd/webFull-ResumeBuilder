660ac92a37b3f688b28c80c14272b5e193703cd6
thefarismd <thefarismd@icloud.com>
2023-09-11 15:13:45 +0800

    Dynamic implementation to disable react & redux dev tool in production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,25 @@ import { Provider } from 'react-redux';
 import './index.css';
 import './bootstrap.min.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import store from './store';
+import {disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+
+/*
+ * NODE_ENV values in CRA (Create React App):
+ * 1. 'development' when using `npm start`
+ * 2. 'test' when using `npm test`
+ * 3. 'production' after using `npm run build`
+ * 
+ * Note: NODE_ENV can't be overridden in `.env` for CRA projects. 
+ * When using `npm start`, NODE_ENV is always 'development'.
+ */
+
+// Disable React DevTools for production
+if (process.env.NODE_ENV.toUpperCase() === 'DEVELOPMENT') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,7 +33,9 @@ root.render(
   </React.StrictMode>
 );
 
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,17 @@ const store = configureStore({
     userRegister: registerReducer,
     userLogin: loginReducer,
   },
+  devTools: process.env.NODE_ENV.toUpperCase() !== 'PRODUCTION',
 });
+
+/*
+ * NODE_ENV values in CRA (Create React App):
+ * 1. 'development' when using `npm start`
+ * 2. 'test' when using `npm test`
+ * 3. 'production' after using `npm run build`
+ * 
+ * Note: NODE_ENV can't be overridden in `.env` for CRA projects. 
+ * When using `npm start`, NODE_ENV is always 'development'.
+ */
 
 export default store;
